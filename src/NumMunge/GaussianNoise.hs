e7c197f45fbd28c5a3d3110c10e31f85b1d8df20
Aistis Raulinaitis <sheganinans@gmail.com>
2025-02-27 03:28:59 -0800

    cleanup

---
+++ src/NumMunge/GaussianNoise.hs
@@ -0,0 +1,278 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module NumMunge.GaussianNoise where
+
+import Clash.Prelude
+import qualified Data.List as L
+import Data.Word (Word32)
+import Text.Printf (printf)
+
+type Fixed16 = SFixed 16 16
+
+type Seed = (Word32, Word32, Word32, Word32)
+
+generateNoise :: (HiddenClockResetEnable dom) => Signal dom (Fixed16, Fixed16)
+generateNoise = boxMuller randomPairs
+
+sqrtFixed :: Fixed16 -> Fixed16
+sqrtFixed x = if x == 0 then 0 else go 5 (if x < 1 then 0.5 else x / 1.414213562)
+  where
+    go :: Int -> Fixed16 -> Fixed16
+    go 0 y = y
+    go i y = go (i - 1) ((y + (x / y)) / 2)
+
+cosFixed :: Fixed16 -> Fixed16
+cosFixed x = 1 - (x2 / 2) + (x4 / 24) - (x6 / 720)
+  where
+    x2 = x * x
+    x4 = x2 * x2
+    x6 = x4 * x2
+
+sinFixed :: Fixed16 -> Fixed16
+sinFixed x = x - (x3 / 6) + (x5 / 120) - (x7 / 5040)
+  where
+    x2 = x * x
+    x3 = x2 * x
+    x4 = x2 * x2
+    x5 = x4 * x
+    x6 = x3 * x3
+    x7 = x6 * x
+
+lnFixed :: Fixed16 -> Fixed16
+lnFixed x
+  | x <= 0 || x == 1 = 0
+  | x < 1 = -(go (1 / x))
+  | otherwise = go x
+  where
+    go :: Fixed16 -> Fixed16
+    go x' = 2 * (t + t3 / 3 + t5 / 5 + t7 / 7)
+      where
+        t = (x' - 1) / (x' + 1)
+        t2 = t * t
+        t3 = t2 * t
+        t4 = t2 * t2
+        t5 = t4 * t
+        t6 = t3 * t3
+        t7 = t6 * t
+
+boxMuller ::
+  (HiddenClockResetEnable dom) =>
+  Signal dom (Fixed16, Fixed16) ->
+  Signal dom (Fixed16, Fixed16)
+boxMuller uniform = bundle (z0, z1)
+  where
+    (u1, u2) = unbundle uniform
+    r = fmap (* 0.5) $ sqrtFixed <$> fmap ((* (-2)) . lnFixed) u1
+    theta = fmap (* 6.28318530718) u2
+    z0 = (*) <$> r <*> fmap cosFixed theta
+    z1 = (*) <$> r <*> fmap sinFixed theta
+
+type State = (Word32, Word32, Word32, Word32)
+
+randomPairs :: (HiddenClockResetEnable dom) => Signal dom (Fixed16, Fixed16)
+randomPairs = bundle (r1, r2)
+  where
+    r1 = randomStream $ initialState 0xDEADBEEF
+    r2 = randomStream $ initialState 0x12345678
+
+initialState :: Word32 -> Seed
+initialState seed = (seed, seed `xor` 0x01234567, seed `xor` 0x89ABCDEF, seed `xor` 0xDEADBEEF)
+
+randomStream :: (HiddenClockResetEnable dom) => Seed -> Signal dom Fixed16
+randomStream initState = fmap toFixed stateSignal
+  where
+    initState = initialState 0xC0FFEE42
+    stateSignal = register initState $ fmap (snd . xoshiro128Plus) stateSignal
+    toFixed state =
+      let (randWord, _) = xoshiro128Plus state
+          scaledVal = fromIntegral ((randWord `shiftR` 16) .&. 0x7FFF) / 32768
+       in scaledVal
+
+xoshiro128Plus :: State -> (Word32, State)
+xoshiro128Plus (s0, s1, s2, s3) =
+  let result = s0 + s3
+      s1' = s1 `xor` (s0 `shiftL` 9)
+      s2' = s2 `xor` (s1 `shiftL` 13)
+      s3' = s3 `xor` (s2 `rotateL` 7)
+      s0' = s0 `xor` (s3 `shiftL` 4)
+      nextState = (s1', s2', s3', s0')
+   in (result, nextState)
+
+{-
+
+  Debugging functions
+
+-}
+
+testFullCircuit :: [(Fixed16, Fixed16)]
+testFullCircuit = simulateN @System 100 circuit (L.repeat ())
+  where
+    circuit :: (HiddenClockResetEnable dom) => Signal dom () -> Signal dom (Fixed16, Fixed16)
+    circuit _ = exposeClockResetEnable generateNoise clockGen resetGen enableGen
+
+debugTest :: IO ()
+debugTest = do
+  putStrLn "Circuit simulation results:"
+  mapM_ printSample $ L.drop 10 $ L.zip [(1 :: Int) ..] testFullCircuit
+  where
+    printSample (i, (x, y)) =
+      printf
+        "Sample %3d: (%8.4f, %8.4f)\n"
+        i
+        (realToFrac x :: Double)
+        (realToFrac y :: Double)
+
+debugState :: IO ()
+debugState = do
+  putStrLn "RNG State transitions:"
+  let testInputs :: [Word32]
+      testInputs = L.take 10 (L.replicate 5 0xDEADBEEF L.++ L.replicate 5 0x12345678)
+
+      initState' = initialState 0xDEADBEEF
+
+      transitions :: [Seed]
+      transitions = L.scanl step initState' testInputs
+
+      outputs :: [Fixed16]
+      outputs = L.zipWith (curry (genOutput . xoshiro128Plus . fst)) transitions (L.tail transitions)
+
+  mapM_ printTransition $ L.zip3 [1 ..] transitions outputs
+  where
+    step :: Seed -> Word32 -> Seed
+    step curSeed _ = snd $ xoshiro128Plus curSeed
+
+    genOutput :: (Word32, Seed) -> Fixed16
+    genOutput (randWord, _) =
+      fromRational $ toRational randWord / toRational (maxBound :: Word32)
+
+    printTransition :: (Int, Seed, Fixed16) -> IO ()
+    printTransition (i, state, val) =
+      printf
+        "Step %3d: State=%s Value=%f\n"
+        i
+        (show state)
+        (realToFrac val :: Double)
+
+debugStateTransition :: IO ()
+debugStateTransition = do
+  putStrLn "State Transition Debugging:"
+  let initState' = initialState 0xDEADBEEF
+      states = L.take 10 $ L.iterate (snd . xoshiro128Plus) initState'
+      values = L.map toFixed states
+  mapM_ printTransition $ L.zip [1 ..] (L.zip states values)
+  where
+    toFixed state =
+      let (randWord, _) = xoshiro128Plus state
+          scaledVal = fromIntegral ((randWord `shiftR` 16) .&. 0x7FFF) / 32768
+       in scaledVal
+
+    printTransition :: (Int, (Seed, Fixed16)) -> IO ()
+    printTransition (i, (state, val)) =
+      printf
+        "Step %3d: State=%s Value=%.6f\n"
+        i
+        (show state)
+        (realToFrac val :: Double)
+
+testRNG :: IO ()
+testRNG = do
+  putStrLn "Testing RNG output:"
+  let initState' = initialState 0xDEADBEEF
+      outputs :: [(Word32, State)]
+      outputs = L.take 10 $ L.iterate (xoshiro128Plus . snd) (xoshiro128Plus initState')
+
+  mapM_ (\(i, (val, _)) -> printf "Output %3d: %08x\n" (i :: Int) val) $
+    L.zip [1 ..] outputs
+
+debugValues :: IO ()
+debugValues = do
+  putStrLn "Random value distribution check:"
+  let values = simulateN @System 20 rngCircuit (L.repeat 0)
+
+  mapM_
+    ( \(i, v) ->
+        printf
+          "Value %2d: %f\n"
+          (i :: Int)
+          (realToFrac v :: Double)
+    )
+    $ L.zip [1 ..] values
+  where
+    rngCircuit ::
+      (HiddenClockResetEnable dom) =>
+      Signal dom Word32 ->
+      Signal dom Fixed16
+    rngCircuit _ =
+      let stateSignal =
+            register initState $
+              fmap (snd . xoshiro128Plus) stateSignal
+          initState = initialState 0xDEADBEEF
+       in fmap genOutput stateSignal
+      where
+        genOutput s =
+          let (randWord, _) = xoshiro128Plus s
+           in fromRational $
+                toRational randWord / toRational (maxBound :: Word32)
+
+testSeed :: [(Fixed16, Fixed16)]
+testSeed = simulateN @System 100 circuit (L.repeat ())
+  where
+    circuit :: (HiddenClockResetEnable dom) => Signal dom () -> Signal dom (Fixed16, Fixed16)
+    circuit _ = exposeClockResetEnable generateNoise clockGen resetGen enableGen
+
+debugBoxMuller :: IO ()
+debugBoxMuller = do
+  putStrLn "Box-Muller transform test:"
+  let circuit :: (HiddenClockResetEnable dom) => Signal dom () -> Signal dom (Fixed16, Fixed16)
+      circuit _ = boxMuller $ bundle (r1, r2)
+        where
+          r1 = register 0.1 $ randomStream s1
+          r2 = register 0.1 $ randomStream s2
+          s1 = initialState 0xDEADBEEF
+          s2 = initialState 0x12345678
+      pairs = simulateN @System 10 circuit (L.repeat ())
+  mapM_
+    ( \(i, (x, y)) ->
+        printf
+          "Pair %2d: (%8.4f, %8.4f)\n"
+          i
+          (realToFrac x :: Double)
+          (realToFrac y :: Double)
+    )
+    $ L.zip [1 :: Int ..] pairs
+
+debugDistribution :: IO ()
+debugDistribution = do
+  putStrLn "Distribution of random values:"
+  let values = L.take 1000 $ simulateN @System 1000 circuit (L.repeat ())
+      ranges = [(i / 10, (i + 1) / 10) | i <- [0 .. 9]]
+      buckets = countBuckets values ranges
+  mapM_ (printBucket ranges) $ L.zip [0 ..] buckets
+  where
+    circuit :: (HiddenClockResetEnable dom) => Signal dom () -> Signal dom Fixed16
+    circuit _ =
+      withClockResetEnable clockGen resetGen enableGen $
+        randomStream (initialState 0xDEADBEEF)
+
+    countBuckets :: [Fixed16] -> [(Fixed16, Fixed16)] -> [Int]
+    countBuckets vs = L.map count
+      where
+        count (l, h) = L.length $ filter (\x -> x >= l && x < h) vs
+
+    printBucket :: [(Fixed16, Fixed16)] -> (Int, Int) -> IO ()
+    printBucket rs (idx, count) =
+      printf "[%.1f-%.1f): %d\n" (realToFrac l :: Double) (realToFrac h :: Double) count
+      where
+        (l, h) = rs L.!! idx
+
+debugRange :: IO ()
+debugRange = do
+  putStrLn "Range verification:"
+  let values = L.take 100 $ simulateN @System 100 circuit (L.repeat ())
+  printf "Min value: %.6f\n" (realToFrac (L.minimum values) :: Double)
+  printf "Max value: %.6f\n" (realToFrac (L.maximum values) :: Double)
+  where
+    circuit :: (HiddenClockResetEnable dom) => Signal dom () -> Signal dom Fixed16
+    circuit _ =
+      withClockResetEnable clockGen resetGen enableGen $
+        randomStream (initialState 0xDEADBEEF)
--- tests/Tests/NumMunge/Project.hs
+++ tests/Tests/NumMunge/Project.hs
@@ -6,14 +6,12 @@ module Tests.NumMunge.Project where
 
 import Clash.Prelude
 import qualified Data.List as L
-import Data.Word (Word32)
-import NumMunge.Project
+import NumMunge.GaussianNoise
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 import Test.Tasty.TH
 
--- Helper for approximate equality
 approxEq :: Fixed16 -> Fixed16 -> Bool
 approxEq x y = abs (x - y) < 0.01
 
@@ -47,70 +45,78 @@ case_xoshiro128Plus = do
 
 case_randomStream :: Assertion
 case_randomStream = do
-  let input = pure (initialState 42)
-      output = sampleN @System 5 (randomStream input)
-  assertBool "Random stream values in [0,1]" $
-    all (\x -> x >= 0 && x <= 1) output
+  let output =
+        sampleN @System 500 $
+          withClockResetEnable clockGen resetGen enableGen $
+            randomStream (initialState 42)
+  assertBool "Random stream values in [0,1)" $
+    all (\x -> x >= 0 && x < 1) output
 
 case_randomPairs :: Assertion
 case_randomPairs = do
   let output =
-        sampleN @System 5 $
+        sampleN @System 500 $
           withClockResetEnable clockGen resetGen enableGen randomPairs
-  assertBool "Random pairs in [0,1]" $
+  assertBool "Random pairs in [0,1)" $
     all (\(x, y) -> x >= 0 && x < 1 && y >= 0 && y < 1) output
 
 case_boxMuller :: Assertion
 case_boxMuller = do
   let input = pure (0.5, 0.5)
-      output = sampleN @System 5 (boxMuller input)
+      output = sampleN @System 500 (boxMuller input)
   assertBool "Box-Muller output in reasonable range" $
     all (\(x, y) -> abs x < 10 && abs y < 10) output
 
 case_topEntity :: Assertion
 case_topEntity = do
-  let clock = clockGen @System
-      reset = resetGen
-      enable = enableGen
-      output = sampleN @System 5 (topEntity clock reset enable)
+  let output =
+        sampleN @System 500 $
+          withClockResetEnable clockGen resetGen enableGen generateNoise
   assertBool "Top entity produces reasonable output" $
     all (\(x, y) -> abs x < 10 && abs y < 10) output
 
 prop_rngRange :: Property
-prop_rngRange = forAll (vector 1000 :: Gen [Word32]) $ \seeds ->
-  let circuit =
-        withClockResetEnable clockGen resetGen enableGen $
-          randomStream . fmap initialState
-      values = simulateN @System 1000 circuit (L.map fromIntegral seeds)
-   in all (\x -> x >= 0 && x < 1) values
+prop_rngRange =
+  property $
+    let circuit =
+          withClockResetEnable clockGen resetGen enableGen $
+            randomStream (initialState 0xDEAD_BEEF)
+        values = sampleN @System 1000 circuit
+     in conjoin $ L.map (\x -> x >= 0 Test.Tasty.QuickCheck..&&. x < 1) values
 
 prop_rngDistribution :: Property
-prop_rngDistribution = forAll (choose (1, maxBound) :: Gen Word32) $ \seed ->
-  let circuit =
-        withClockResetEnable clockGen resetGen enableGen $
-          randomStream . fmap initialState
-      values = L.take 10_000 $ simulateN @System 10_000 circuit (L.repeat seed)
-      buckets = countInBuckets values
-      expected = fromIntegral (L.length values `div` 10) :: Double
-      chiSquare =
-        L.sum
-          [ fromIntegral ((observed - expected') * (observed - expected')) / expected
-            | let expected' = round expected,
-              observed <- buckets
-          ]
-   in chiSquare < 16.919 -- 95% confidence for 9 degrees of freedom
+prop_rngDistribution =
+  property $
+    let circuit =
+          withClockResetEnable clockGen resetGen enableGen $
+            randomStream (initialState 0xDEAD_BEEF)
+        values = sampleN @System 10_000 circuit
+        buckets = countInBuckets values
+        expected = fromIntegral (L.length values `div` 10) :: Double
+        chiSquare =
+          sum
+            [ fromIntegral ((observed - expected') * (observed - expected')) / expected
+              | let expected' = round expected,
+                observed <- buckets
+            ]
+     in counterexample ("Chi-square value: " L.++ show chiSquare) $
+          chiSquare < 16.919 -- 95% confidence for 9 degrees of freedom
   where
     countInBuckets vs = L.map count [0 .. 9]
       where
-        count i = L.length $ filter (\x -> x >= i / 10 && x < (i + 1) / 10) vs
+        count i = L.length $ L.filter (\x -> x >= i / 10 && x < (i + 1) / 10) vs
 
 prop_rngPeriod :: Property
-prop_rngPeriod = forAll (choose (1, maxBound) :: Gen Word32) $ \seed ->
-  let circuit =
-        withClockResetEnable clockGen resetGen enableGen $
-          randomStream . fmap initialState
-      values = L.take 1000 $ simulateN @System 1000 circuit (L.repeat seed)
-   in L.length (L.nub values) > 900
+prop_rngPeriod =
+  property $
+    let circuit =
+          withClockResetEnable clockGen resetGen enableGen $
+            randomStream (initialState 0xDEAD_BEEF)
+        values = sampleN @System 1000 circuit
+        uniqueCount = L.length (L.nub values)
+     in counterexample
+          ("Unique values: " L.++ show uniqueCount L.++ " out of 1000")
+          $ uniqueCount > 900
 
 tests :: TestTree
 tests = $(testGroupGenerator)
